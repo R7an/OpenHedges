d63117d808cc9c7a653639b5c758f84062927386
Swelly <Surfswell17@gmail.com>
2018-07-23 16:46:04 -0700

    new branch - header and footer dummy content in

--- client/js/app/components/Footer.js
+++ client/js/app/components/Footer.js
@@ -2,8 +2,43 @@ import React from 'react';
 
 const Footer = () => (
     <footer>
-        <hr />
-        <p>Footer</p>
+        <div className="footer-inner">
+        	<div className="footer-item">
+        		<div className="avatar"></div>
+        		<div className="item-text-container">
+        			<div className="item-text">Someone invested X</div>
+        			<div className="item-time">11 hours ago</div>
+        		</div>
+        	</div>
+        	<div className="footer-item">
+        		<div className="avatar"></div>
+        		<div className="item-text-container">
+        			<div className="item-text">Someone invested X</div>
+        			<div className="item-time">18 hours ago</div>
+        		</div>
+        	</div>
+        	<div className="footer-item">
+        		<div className="avatar"></div>
+        		<div className="item-text-container">
+        			<div className="item-text">Someone invested X</div>
+        			<div className="item-time">22 hours ago</div>
+        		</div>
+        	</div>
+        	<div className="footer-item">
+        		<div className="avatar"></div>
+        		<div className="item-text-container">
+        			<div className="item-text">Someone invested X</div>
+        			<div className="item-time">2 days ago</div>
+        		</div>
+        	</div>
+        	<div className="footer-item">
+        		<div className="avatar"></div>
+        		<div className="item-text-container">
+        			<div className="item-text">Someone invested X</div>
+        			<div className="item-time">2 days ago</div>
+        		</div>
+        	</div>
+        </div>
     </footer>
 );
 
--- client/js/app/components/Header.js
+++ client/js/app/components/Header.js
@@ -1,24 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Header = () => (
-    <header>
-        <Link to="/">Home</Link>
-        <br/>
-        <Link to="/basic-permission">Basic Permission Test</Link>
-        <br/>
-        <Link to="/advanced-permission">Advanced Permission Test</Link>
-        <br/>
-        <Link to="/login">Login</Link>
-        <br/>
-        <Link to="/signup">Signup</Link>
-        <br/>
-        <Link to="/logout">Logout</Link>
-        <br/>
-
-        <hr />
-    </header>
+	<header>
+		<Link to="/" classname="nav-item logo-link">
+			OpenHedges
+		</Link>
+		
+		<div className="header-links">
+			<Link to="/concept" classname="nav-item">Concept</Link>
+			<Link to="/rules" classname="nav-item">Rules</Link>
+			<Link to="/blog" classname="nav-item">Blog</Link>
+			<Link to="/sign-up" classname="nav-item btn btn-sign-up">Sign Up</Link>
+		</div>
+	</header>
 );
 
 export default Header;
