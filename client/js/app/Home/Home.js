1b7962064b50990de998487e95ddf3e49e9b5fae
Swelly <Surfswell17@gmail.com>
2018-07-23 17:05:42 -0700

    landing content done

--- client/js/app/Home/Home.js
+++ client/js/app/Home/Home.js
@@ -1,22 +1,41 @@
-import React from 'react';
+import React from "react";
 // import axios from 'src/common/myAxios';
 
 class Home extends React.Component {
-    constructor(props) {
-        super(props);
+	constructor(props) {
+		super(props);
 
-        this.state = {};
-    }
+		this.state = {};
+	}
 
-    componentDidMount() {}
+	componentDidMount() {}
 
-    render() {
-        return (
-            <div className="home-page">
-                <h1>OpenHedges.Co</h1>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="home-page">
+				<div className="hero-container">
+					<div className="super-text-container">
+						<div className="logo" />
+						<p>Futures</p>
+						<p>Stock</p>
+						<p>Digital</p>
+					</div>
+					<h1>Best Ideas Investment Platform</h1>
+					<h4>
+						Asset class agnostic – lorem ipsum dolor sit amet,
+						consectetur adipisicing elit.
+					</h4>
+					<button className="btn cta-button">Request Alpha Access</button>
+				</div>
+				<div className="signup-animator">
+					<h3>Someone Signed Up</h3>
+					<h3>Someone Else Signed Up</h3>
+					<h3>Someone Else-else Signed Up</h3>
+					<h3>Someone Else-else-else Signed Up</h3>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Home;
--- client/js/app/components/Header.js
+++ client/js/app/components/Header.js
@@ -7,12 +7,12 @@ const Header = () => (
 		<Link to="/" classname="nav-item logo-link">
 			OpenHedges
 		</Link>
-		
+
 		<div className="header-links">
 			<Link to="/concept" classname="nav-item">Concept</Link>
 			<Link to="/rules" classname="nav-item">Rules</Link>
 			<Link to="/blog" classname="nav-item">Blog</Link>
-			<Link to="/sign-up" classname="nav-item btn btn-sign-up">Sign Up</Link>
+			<button classname="nav-item btn btn-sign-up">Sign Up</button>
 		</div>
 	</header>
 );
