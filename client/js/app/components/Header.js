c656edd2643ed2eeb97306f34782e7cfcfc97052
Swelly <Surfswell17@gmail.com>
2018-07-23 17:32:55 -0700

    Variables file started for importer

--- client/js/app/components/Header.js
+++ client/js/app/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const Header = () => (
 	<header>
-		<Link to="/" classname="nav-item logo-link">
+		<Link to="/" className="nav-item logo-link">
 			OpenHedges
 		</Link>
 
