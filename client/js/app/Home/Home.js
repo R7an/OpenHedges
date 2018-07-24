de81e3fbd9463a7007a029a6281d0236ccbd841b
Swelly <Surfswell17@gmail.com>
2018-07-24 15:38:54 -0700

    hero section

--- client/js/app/Home/Home.js
+++ client/js/app/Home/Home.js
@@ -13,25 +13,24 @@ class Home extends React.Component {
 	render() {
 		return (
 			<div className="landing-page">
-				<div className="hero-container">
-					<div className="super-text-container">
-						<div className="logo" />
-						<p>Futures</p>
-						<p>Stock</p>
-						<p>Digital</p>
+				<div className="landing-inner">
+					<div className="hero-container">
+						<div className="super-text-container">
+							<div className="logo"></div>
+							<p>Futures</p>
+							<p>Stock</p>
+							<p>Digital</p>
+						</div>
+						<h1>Best Ideas Investment Platform</h1>
+						<h4>
+							Asset class agnostic – lorem ipsum dolor sit amet,
+							consectetur adipisicing elit.
+						</h4>
+						<button className="btn cta-button">Request Alpha Access</button>
 					</div>
-					<h1>Best Ideas Investment Platform</h1>
-					<h4>
-						Asset class agnostic – lorem ipsum dolor sit amet,
-						consectetur adipisicing elit.
-					</h4>
-					<button className="btn cta-button">Request Alpha Access</button>
-				</div>
-				<div className="signup-animator">
-					<h3>Someone Signed Up</h3>
-					<h3>Someone Else Signed Up</h3>
-					<h3>Someone Else-else Signed Up</h3>
-					<h3>Someone Else-else-else Signed Up</h3>
+					<div className="hero-graphic">
+						<div>test</div>
+					</div>
 				</div>
 			</div>
 		);
