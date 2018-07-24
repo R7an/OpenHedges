069fdb2e311d841b5bcb3d1df30d3a188fdb63c1
Swelly <Surfswell17@gmail.com>
2018-07-24 13:33:48 -0700

    header

--- client/js/app/components/Header.js
+++ client/js/app/components/Header.js
@@ -4,15 +4,17 @@ import { Link } from "react-router-dom";
 
 const Header = () => (
 	<header>
-		<Link to="/" className="nav-item logo-link">
-			OpenHedges
-		</Link>
+		<div className="header-inner">
+			<Link to="/" className="nav-item logo-link">
+				OpenHedges
+			</Link>
 
-		<div className="header-links">
-			<Link to="/concept" className="nav-item">Concept</Link>
-			<Link to="/rules" className="nav-item">Rules</Link>
-			<Link to="/blog" className="nav-item">Blog</Link>
-			<button className="nav-item btn btn-sign-up">Sign Up</button>
+			<div className="header-links">
+				<Link to="/concept" className="nav-item">Concept</Link>
+				<Link to="/rules" className="nav-item">Rules</Link>
+				<Link to="/blog" className="nav-item">Blog</Link>
+				<button className="nav-item btn btn-sign-up">Sign Up</button>
+			</div>
 		</div>
 	</header>
 );
