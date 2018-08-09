fb78a04008058c7bd0f4a2ca71d28dfcb20a1680
Swelly <Surfswell17@gmail.com>
2018-08-08 18:05:30 -0700

    modal styled

--- client/src/app/Home/components/SignupModal.js
+++ client/src/app/Home/components/SignupModal.js
@@ -1,54 +1,48 @@
-import React from 'react';
+import React from "react";
 
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
-import { Dialog, Classes, Tooltip, Button } from "@blueprintjs/core";
+import { Dialog, Classes, Button } from "@blueprintjs/core";
 
-import { closeSignupModal } from '../../../redux/actions/UiActions';
+import { closeSignupModal } from "../../../redux/actions/UiActions";
 
-import SignupForm from '../forms/SignupForm';
-import UserOperations from '../../../redux/operations/UserOperations';
+import SignupForm from "../forms/SignupForm";
+import UserOperations from "../../../redux/operations/UserOperations";
 
-const {signupUser} = UserOperations;
-const ConnectedSignupModal = ({ closeSignupModal, isSignupModalOpen, signupUser }) => (
+const { signupUser } = UserOperations;
+const ConnectedSignupModal = ({
+    closeSignupModal,
+    isSignupModalOpen,
+    signupUser
+}) => (
     <Dialog
         autoFocus={true}
-        title={'Sign-Up'}
+        title={"Sign Up"}
         canEscapeKeyClose={true}
+        isCloseButtonShown={false}
         canOutsideClickClose={true}
         enforceFocus={true}
         usePortal={false}
         lazy={true}
         onClose={() => closeSignupModal(false)}
-        isOpen={isSignupModalOpen}>
+        isOpen={isSignupModalOpen}
+    >
         <div className={Classes.DIALOG_BODY}>
-            <SignupForm signupUser = {signupUser} />
-        </div>
-        <div className={Classes.DIALOG_FOOTER}>
-            <div className={Classes.DIALOG_FOOTER_ACTIONS}>
-                <Tooltip content="This button is hooked up to close the dialog.">
-                    <Button onClick={() => closeSignupModal(false)}>Close</Button>
-                </Tooltip>
-            </div>
+            <SignupForm signupUser={signupUser} />
         </div>
     </Dialog>
 );
 
-const mapStateToProps = state => (
-    {
-        isSignupModalOpen: state.isSignupModalOpen,
-    }
-)
-const mapDispatchToProps = dispatch => (
-    {
-        closeSignupModal: (bool) => dispatch(closeSignupModal(bool)),
-        signupUser: (user) => dispatch(signupUser(user)),
-    }
-)
+const mapStateToProps = state => ({
+    isSignupModalOpen: state.isSignupModalOpen
+});
+const mapDispatchToProps = dispatch => ({
+    closeSignupModal: bool => dispatch(closeSignupModal(bool)),
+    signupUser: user => dispatch(signupUser(user))
+});
 
-const SignupModal = connect(mapStateToProps, mapDispatchToProps)(ConnectedSignupModal);
+const SignupModal = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(ConnectedSignupModal);
 export default SignupModal;
-
-
-
-
--- client/src/app/Home/forms/SignupForm.js
+++ client/src/app/Home/forms/SignupForm.js
@@ -1,73 +1,88 @@
-import React from 'react';
-import { reduxForm, Field } from 'redux-form'
-import PasswordValidator from 'password-validator';
-import emailValidator from 'email-validator';
+import React from "react";
+import { reduxForm, Field } from "redux-form";
+import PasswordValidator from "password-validator";
+import emailValidator from "email-validator";
 
 const passwordSchema = new PasswordValidator();
 
-passwordSchema
-    .is().min(6);
+passwordSchema.is().min(6);
 
-const validate = (values) => {
+const validate = values => {
     const errors = {};
     if (!values.email) {
-        errors.email = 'Please enter your email.';
+        errors.email = "Please enter your email.";
     } else if (!emailValidator.validate(values.email)) {
-        errors.email = 'Please enter a correct email address.';
+        errors.email = "Please enter a correct email address.";
     }
     if (!values.fullName) {
-        errors.fullName = 'Please enter your full name.';
+        errors.fullName = "Please enter your full name.";
     }
     if (!values.username) {
-        errors.username = 'Please enter a username.';
+        errors.username = "Please enter a username.";
     }
     if (!values.password) {
-        errors.password = 'Please enter a password.';
+        errors.password = "Please enter a password.";
     } else if (!passwordSchema.validate(values.password)) {
-        errors.password = 'Password is too short, minimum is 6 characters.';
+        errors.password = "Password is too short, minimum is 6 characters.";
     }
     if (!values.confirmPassword) {
-        errors.confirmPassword = 'Please confirm your password by retyping it.';
+        errors.confirmPassword = "Please confirm your password by retyping it.";
     } else if (!(values.confirmPassword === values.password)) {
-        errors.confirmPassword = 'The password you have entered does not match the one above.'
+        errors.confirmPassword =
+            "The password you have entered does not match the one above.";
     }
     return errors;
 };
 
-const createRenderer = render => ({ input, meta, label, type, ...rest }) =>
-    <label className={meta.error && meta.touched ? 'error' : ''}>
-        <div style={{ "display": "flex" }}>
-            <label style={{ "textAlign": "left" }}>{label}</label>
-            {' '}
+const createRenderer = render => ({ input, meta, label, type, ...rest }) => (
+    <label className={meta.error && meta.touched ? "error" : ""}>
+        <div className="popover-label">
+            {label}
             {render(input, label, type, rest)}
-        </div>
+        </div>{" "}
+        
         {meta.error &&
-            meta.touched &&
-            <span style={{ "color": "red", "fontSize": "12px" }}>
-                {meta.error}
-            </span>}
+            meta.touched && <span className="meta-error">{meta.error}</span>}
     </label>
+);
 
-const RenderInput = createRenderer((input, label, type) =>
-    <input {...input} placeholder={label} type={type} className="pt-input" />
-)
+const RenderInput = createRenderer((input, label, type) => (
+    <input {...input} type={type} className="pt-input" />
+));
 
-let SignUpForm = ({handleSubmit, submitting, signupUser  }) =>
-    <form style={{ textAlign: "center" }} onSubmit={handleSubmit(signupUser)}>
+let SignUpForm = ({ handleSubmit, submitting, signupUser }) => (
+    <form onSubmit={handleSubmit(signupUser)}>
         <div className="pt-label-container">
-            <Field name="email" label="Email" component={RenderInput} />
-            <Field name="fullName" label="Full Name" component={RenderInput} />
-            <Field name="username" label="Username" component={RenderInput} />
-            <Field name="password" type="password" label="Password" component={RenderInput} />
-            <Field name="confirmPassword" type="password" label="Confirm Password" component={RenderInput} />
-            <button type="submit" disabled={submitting} className="button button-primary">Submit</button>
+            <Field name="firstName" label="First Name" component={RenderInput} />
+            <Field name="lastName" label="Last Name" component={RenderInput} />
+            <Field name="email" label="Email Address" component={RenderInput} />
+            <Field
+                name="password"
+                type="password"
+                label="Password"
+                component={RenderInput}
+            />
+            <Field
+                name="confirmPassword"
+                type="password"
+                label="Confirm Password"
+                component={RenderInput}
+            />
         </div>
+        <button
+            type="submit"
+            disabled={submitting}
+            className="button button-primary"
+        >
+            Submit
+        </button>
     </form>
+);
 
 SignUpForm = reduxForm({
-    form: 'signUpForm',
+    form: "signUpForm",
     destroyOnUnmount: false,
-    validate,
+    validate
 })(SignUpForm);
 
 export default SignUpForm;
